97686912565b02680e1ce8a0960000762f5525cf
Venegaam <67711768+Venegaam@users.noreply.github.com>
2020-08-18 12:34:27 -0700

    Add files via upload

---
+++ js/os-identify.js
@@ -0,0 +1,18 @@
+var os_name = "Unknown OS"
+
+if (navigator.userAgent.indexOf("Win") != -1) os_name = "Windows OS";
+if (navigator.userAgent.indexOf("Mac") != -1) os_name = "Macintosh";
+if (navigator.userAgent.indexOf("Linux") != -1) os_name = "Linux OS";
+if (navigator.userAgent.indexOf("Android") != -1) os_name = "Android OS";
+if (navigator.userAgent.indexOf("like Mac") != -1) os_name = "iOS";
+
+console.log(os_name);
+
+if (os_name == "Android OS" || os_name == "iOS" || os_name == "Windows OS") {
+
+var els = document.getElementById("content").firstChild.nextSibling;
+
+console.log(els);
+
+els.style.width = "90%";
+}
